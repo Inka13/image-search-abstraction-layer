0515ef621c81ec13b5e57af3cfb82aea3a8e838b
Glitch (image-search-inka) <none>
2018-01-09 19:18:59 +0000

    🏹📕 Checkpoint server.js:617097/430

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 const express = require('express');
-const app = express();
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const mongodb = require("mongodb");
-//const mongoose = require('mongoose');
 const MongoClient = mongodb.MongoClient;
 const mongoUrl = process.env.MONGOLAB_URI;
-const key = process.env.API_KEY;
 const searchModel = require("./searchModel.js");
-
 const pixabay = require('pixabay-api');
+const key = process.env.API_KEY;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-
-
 app.get("/api/imagesearch/:keywords*", function (request, response, next) {
-  
   var {keywords} = request.params;
   var {offset} = request.query;
-  //console.log(offset);
-  //console.log(keywords);
   var data = new searchModel({
     keywords,
     date: new Date()
@@ -34,46 +25,51 @@ app.get("/api/imagesearch/:keywords*", function (request, response, next) {
     if(err) {
       console.log('Unable to connect to database...');
       throw err;
-    } else console.log('Connected...');
-    /*db.collection('search').insert(data, (err, res) => {
+    }
+    db.collection('search').insert(data, (err, res) => {
       if(err) {
         console.log('Unable to insert to database...');
         throw err;
-    } else {
-      console.log('Inserted...');
-      
-    }
-    }); */
+      }
+    }); 
   });
-  
+  // search keywords with pixabay-api
   pixabay.searchImages(key, keywords).then((res, err) => {
     let resArr = [];
-    console.log(+offset +10);
     if(!offset) offset=0;
+    // store 10 results to resArr, starting from offset position
     for(let i=+offset; i<(+offset +10); i++){
       let snippet = res.hits[i].pageURL.split('').splice(23);
-      snippet.slice(7);
+      snippet = snippet.slice(0,snippet.length-9).join('');
       resArr.push({
-        url: res.hits[i].pageURL,
+        url: res.hits[i].webformatURL,
         thumbnail: res.hits[i].previewURL,
-        snippet: snippet
+        snippet
       })
     }
     response.json(resArr);
   });
-  
-  
+});
+app.get("/api/latest/imagesearch", function (request, response, next) {
+  MongoClient.connect(mongoUrl, function (err, db) {
+    if(err) {
+      console.log('Unable to connect to database...');
+      throw err;
+    }
+    db.collection('search').find().sort({date}).limit(10, (err, res) => {
+      if(err) {
+        console.log('Unable to search database...');
+        throw err;
+      }
+    }); 
+  });
 });
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  
-  response.sendStatus(200);
-});
+
 
 
 // listen for requests :)
