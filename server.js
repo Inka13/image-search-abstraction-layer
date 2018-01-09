ef9d8b4b30051970499ad1d94c0d5e34d0f6896d
Glitch (image-search-inka) <none>
2018-01-09 13:23:15 +0000

    👫🌵 Checkpoint README.md:617097/49 server.js:617097/5 public/client.js:617097/1 public/style.css:617097/1 views/index.html:617097/4 package.json:617097/79

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+
+/
+app.use(express.static('public'));
+
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
